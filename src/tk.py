30367db9529ff1c09c784e77225770ca570a3172
Ashlc <isadorabpaz@gmail.com>
2024-03-13 19:10:34 -0300

    :sparkles: feat: Implemented Token class

--- src/tk.py
+++ src/tk.py
@@ -1,11 +1,8 @@
 class Token:
+    def __init__(self, token_type, value=None, position=None):
+        self.type = token_type  # Type of the token (e.g., keyword, identifier, operator)
+        self.value = value      # Value of the token (e.g., the actual keyword, identifier name, or operator symbol)
+        self.position = position  # Position of the token in the source code (e.g., line number, column number)
 
-    def __init__(self, category, lexeme, row, col):
-
-        self.category = category
-        self.lexeme = lexeme
-        self.row = row
-        self.col = col
-
-    def to_str(self):
-        return f"              [{(self.row - 1):04d}, {(self.col):04d}] ({(self.category.value):04d}, {(self.category.name):>20}) {{{self.lexeme}}}"
+    def __repr__(self):
+        return f"Token({self.type}, {self.value})"
