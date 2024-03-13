af474ffec0eb01b2c08353208a6d38b2e7f5cd8f
Ashlc <isadorabpaz@gmail.com>
2024-03-13 19:00:42 -0300

    :bug: fix: Removing protected title

---
+++ src/tk.py
@@ -0,0 +1,11 @@
+class Token:
+
+    def __init__(self, category, lexeme, row, col):
+
+        self.category = category
+        self.lexeme = lexeme
+        self.row = row
+        self.col = col
+
+    def to_str(self):
+        return f"              [{(self.row - 1):04d}, {(self.col):04d}] ({(self.category.value):04d}, {(self.category.name):>20}) {{{self.lexeme}}}"
