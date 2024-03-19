18c34df46cd9b1d9e1691c4b8762733d378a866e
Isadora Paz <85000060+Ashlc@users.noreply.github.com>
2024-03-19 11:41:50 -0300

    :construction: Infinite loop on blocks (scope issue)

--- src/semantic.py
+++ src/semantic.py
@@ -1,25 +1,46 @@
 from _parser import Parser
 from enum_tokens import TokenEnums as en
 from lexer import Lexer
+from syntax_tree import SyntaxNode
+
 
 class Semantics:
     def __init__(self):
-        self.environments = [{}]
-        self.types = [{}]
+        self.global_env = {}
+        self.local_envs = [{}]
+        # self.environments = [{}]
 
     def enter_scope(self):
-        self.environments.append({})
-        self.types.append({})
+        # Create a new empty environment for variables in the new scope
+        self.local_envs.append({})
 
     def exit_scope(self):
-        self.environments.pop()
-        self.types.pop()
+        # Remove the current environment when exiting a scope
+        self.local_envs.pop()
+
+    def update_global_variable(self, name, value, var_type):
+        self.global_env[name] = {"type": var_type, "value": value}
+
+    def get_operands(self, node):
+        left = self.visit(node.children[0])
+        right = self.visit(node.children[1])
+
+        if not left.node_type == en.RW_INT or not right.node_type == en.RW_INT:
+            raise Exception("Type error: both operands must be integers")
+        return {"left": left.value, "right": right.value}
 
     def visit(self, node):
-        method_name = f'visit_{node.node_type.name}'
+        method_name = f"visit_{node.node_type.name}"
         print(f"Visiting {node.node_type.name}")
-        if method_name.replace('visit_', '') in ('OP_GT', 'OP_LT', 'OP_GTE', 'OP_LTE', 'OP_EQ', 'OP_NEQ'):
-            method_name = 'visit_comparison'
+        if method_name.replace("visit_", "") in (
+            "OP_GT",
+            "OP_LT",
+            "OP_GTE",
+            "OP_LTE",
+            "OP_EQ",
+            "OP_NEQ",
+        ):
+            method_name = "visit_comparison"
         method = getattr(self, method_name, self.no_visit_method)
         return method(node)
 
@@ -49,66 +70,95 @@ class Semantics:
     def visit_RW_C_CHANNEL(self, node):
         self.current_type = en.RW_C_CHANNEL
         self.visit_children(node)
-        
+
     def visit_RW_PRINT(self, node):
-        value = self.visit(node.children[0])
-        print(f"Printing: {value}")
+        value = self.visit(node.children[0]).value
+        print(f"      [OUTPUT]: {value}")
 
     def visit_OP_ASSIGN(self, node):
-        print("[OP_ASSIGN NODE TREE]")
-        node.print_tree()
-        name = node.children[0].value  # Get the name from the identifier node
-        value = node.children[1].value
-        
-        # if node.children:
-        #     name = node.children[0].value  # Get the name from the identifier node
-        #     print(f"[OP_ASSIGN] Variable name: {name}")
-        #     print(f"[OP_ASSIGN] Checking node children")
-        #     print(node.children)
-        #     id_node = self.visit(node.children[0])
-        #     print("[OP_ASSIGN] IDENTIFIER TREE")
-        #     id_node.print_tree()
-            
-        #     if id_node is not None:
-        #         print(f"Value: {id_node.children[1].value}")
-        #         value = self.visit(node.children[1])
+        name = node.children[0].value
+        value_node = node.children[1]
 
-        if name in self.types[-1] and self.types[-1][name] != self.current_type:
-            raise Exception(f"Type error: variable '{name}' was declared as {self.types[-1][name]} but assigned a value of type {self.current_type}")
+        if (
+            value_node.node_type is not en.NUM
+            and value_node.node_type is not en.STRING_LITERAL
+        ):
+            value_node = self.visit(value_node)
 
-        print(f"Assigning {value} to {name}")
-        
-        self.environments[-1][name] = value
-        self.types[-1][name] = self.current_type
+        print(value_node, value_node.node_type, value_node.value)
+        value = value_node.value
+        var_type = value_node.node_type
+
+        # Check if the variable is in the global environment
+        if name in self.global_env:
+            self.update_global_variable(name, value, var_type)
+        else:
+            self.local_envs[-1][name] = {"type": var_type, "value": value}
 
     def visit_ID(self, node):
-        node.print_tree()
-        name = node.value  # Get the name from the ID node
-        for env in reversed(self.environments):
+        name = node.value
+
+        # Search for the variable in local environments first
+        for env in reversed(self.local_envs):
             if name in env:
-                print(f"Found {name} in environment, returning value: {env[name]}")
-                return env[name]
+                found_var = env[name]
+                print(f"Found value: {found_var}")
+                if isinstance(found_var["value"], int):
+                    return SyntaxNode(en.RW_INT, found_var["value"])
+                elif isinstance(found_var["value"], str):
+                    return SyntaxNode(en.STRING_LITERAL, found_var["value"])
+                else:
+                    return SyntaxNode(env[name]["type"], env[name]["value"])
+
+        # If not found in local environments, check global environment
+        if name in self.global_env:
+            return SyntaxNode(
+                self.global_env[name]["type"], self.global_env[name]["value"]
+            )
+
         raise Exception(f"NameError: name '{name}' is not defined")
 
     def visit_NUM(self, node):
-        return en.RW_INT
+        if isinstance(node.value, int):
+            return SyntaxNode(en.RW_INT, node.value)
+        raise Exception("Type error: expected integer")
 
     def visit_STRING_LITERAL(self, node):
-        return en.RW_INT
-    
+        if isinstance(node.value, str):
+            return SyntaxNode(en.STRING_LITERAL, node.value)
+        else:
+            raise Exception("Type error: expected string")
+
     def visit_BLOCK(self, node):
+        # Save the previous global environment for restoration after the block
+        prev_global_env = self.global_env.copy()
+
         self.enter_scope()
         for statement_node in node.children:
             self.visit(statement_node)
         self.exit_scope()
 
+        # Update global variables with changes made within the block
+        for name, value in self.global_env.items():
+            if name not in prev_global_env or prev_global_env[name] != value:
+                self.update_global_variable(name, value)
+
+    def visit_OP_MULTIPLY(self, node):
+        op = self.get_operands(node)
+        return SyntaxNode(en.NUM, op["left"] * op["right"])
+
+    def visit_OP_DIVIDE(self, node):
+        op = self.get_operands(node)
+        return SyntaxNode(en.NUM, op["left"] / op["right"])
+
     def visit_OP_PLUS(self, node):
-        left = self.visit(node.children[0])
-        right = self.visit(node.children[1])
-        if not left == en.RW_INT or not right== en.RW_INT:
-            raise Exception("Type error: both operands must be integers")
-        return en.RW_INT
-    
+        op = self.get_operands(node)
+        return SyntaxNode(en.NUM, op["left"] + op["right"])
+
+    def visit_OP_MINUS(self, node):
+        op = self.get_operands(node)
+        return SyntaxNode(en.NUM, op["left"] - op["right"])
+
     def visit_RW_WHILE(self, node):
         node.print_tree()
         condition_node = node.children[0]
@@ -117,30 +167,17 @@ class Semantics:
         while self.visit(condition_node):
             self.visit(block_node)
 
-    def visit_OP_MINUS(self, node):
-        left = self.visit(node.children[0])
-        right = self.visit(node.children[1])
-        if not left == en.RW_INT or not right== en.RW_INT:
-            raise Exception("Type error: both operands must be integers")
-        return en.RW_INT
-
     def visit_comparison(self, node):
         node.print_tree()
-        left_type = self.visit(node.children[0])
-        right_type = self.visit(node.children[1])
-        
-        print(f"Left type: {left_type}")
-        print(f"Right type: {right_type}")
-                    
-        if isinstance(left_type, int):
-            left_type = en.RW_INT
-            
-        if isinstance(right_type, int):
-            right_type = en.RW_INT
-                                
-        if left_type == right_type:
+        left = self.visit(node.children[0])
+        right = self.visit(node.children[1])
+
+        print(f"Left: {left}")
+        print(f"Right: {right}")
+
+        if left.node_type == right.node_type:
             return en.RW_BOOL
-        
+
         else:
             raise Exception("Type error: both operands must be of the same type")
 
@@ -161,16 +198,23 @@ class Semantics:
             raise Exception("Type error: condition in 'if' statement must be boolean")
 
 
-
-parser = Parser("""
+parser = Parser(
+    """
     int n = 5;
     int resultado = 1;
+    n = n + 1;
+    print(n);
     while (n > 1){
         resultado = n * resultado;
         n = n - 1;
+        print(n);
+        int a = 1;
+        a = 2;
+        print(a);
     }
     print(resultado);
-""")
+"""
+)
 
 tree = parser.parse()
 tree.print_tree()
